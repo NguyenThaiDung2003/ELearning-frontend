9cb4a990b4fe1b2b9b597b90c1e6ba48c2797910
DungTA <dungkidx01@gmail.com>
2025-03-26 23:05:26 +0700

    Login,Register

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage.jsx";
 import LoginPage from "../pages/Login/LoginPage.jsx";
-
+import RegisterPage from "../pages/Register/RegisterPage.jsx";
 
 const AppRoutes = () => {
   return (
@@ -9,6 +9,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         {/* Thêm các route khác ở đây */}
       </Routes>
     </Router>

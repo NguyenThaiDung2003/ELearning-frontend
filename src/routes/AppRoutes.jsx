d8de02682041395088546195b23946cdeeac1657
dunhdonh <thuydung22004@gmail.com>
2025-05-11 00:51:22 +0700

    1 chut xiu

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import HomePage from "../pages/HomePage/HomePage.jsx";
 import LoginPage from "../pages/Login/LoginPage.jsx";
 import RegisterPage from "../pages/Register/RegisterPage.jsx";
 import ProfilePage from "../pages/ProfilePage/ProfilePage.jsx";
+import AdminDashboard from "../pages/AdminDashboard/AdminDashboard.jsx";
 
 const AppRoutes = () => {
   return (
@@ -12,6 +13,7 @@ const AppRoutes = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/admin" element={<AdminDashboard />} />
         {/* Thêm các route khác ở đây */}
       </Routes>
     </Router>
